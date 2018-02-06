43cae2203e9d62832a2696052d4838f30f6cbfb1
nxg9997 <nxg9997@rit.edu>
2018-02-06 00:26:39 -0500

    changed icon

--- code.js
+++ code.js
@@ -12,7 +12,7 @@ function downloadClip(){
 }
 
 function AddButton(){
-    let btnHTML = '<div class="flex__item flex-shrink-0"><a class="button button--hollow button--icon block clips-button ellipsis"><figure class="icon"><svg class="svg-heart" height="16px" version="1.1" viewBox="0 0 16 16" width="16px" x="0px" y="0px"><path clip-rule="evenodd" d="M8,14L1,7V4l2-2h3l2,2l2-2h3l2,2v3L8,14z" fill-rule="evenodd"></path></svg></figure><span class="follow-btn--text clip-downloader">Download</span></a></div>';
+    let btnHTML = '<div class="flex__item flex-shrink-0"><a class="button button--hollow button--icon block clips-button ellipsis"><figure class="icon"><path clip-rule="evenodd" d="M8,14L1,7V4l2-2h3l2,2l2-2h3l2,2v3L8,14z" fill-rule="evenodd"></path></svg></figure><span class="follow-btn--text clip-downloader">Download</span></a></div>';
 
     let btn = document.createElement('div');
     btn.innerHTML = btnHTML;
